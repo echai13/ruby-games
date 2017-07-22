a02208a7ead6b618ca718c4cb8f9f6cceacc8fc5
echai13 <cher13@brandeis.edu>
2017-07-22 00:38:09 -0400

    edit games

--- Riddles.rb
+++ Riddles.rb
@@ -1,12 +1,12 @@
-class Riddles 
+class Riddles
   $riddle_bank_A = ["What can travel around the world while staying in a corner?",
-                  "What gets wetter and wetter the more it dries?"]
+    "What gets wetter and wetter the more it dries?"]
 
   $riddle_bank_B = ["Which word in the dictionary is spelled incorrectly?",
-                  "The more you take, the more you leave behind, what am I?"]
+      "The more you take, the more you leave behind, what am I?"]
 
   $riddle_bank_C = ["Feed me and I will live, yet give me a drink and I die, what am I?",
-                  "What gets broken without being held?"]
+        "What gets broken without being held?"]
 
   $answers = ["stamp", "towel", "incorrectly", "footsteps", "fire", "silence"]
   $round = 1
@@ -17,19 +17,20 @@ class Riddles
     puts "As a guest, you are invited to determine the "
     puts "fate of a suspected mafia. The town will hang the man "
     puts "if you can't guess the answer before you used up all "
-    puts "your tries. If the man is killed, you will be next..."
+    puts "your 5 tries. If the man is killed, you will be next..."
     puts ""
 
 
     tries = 5
     correct = false
-    while !correct || tries > 0
+    while !correct && tries > 0
       get_riddles()
       print "What is your answer? "
       input = $stdin.gets.chomp
       correct = check_answer(input)
+      tries -= 1
 
-      if correct && $round != 2
+      if correct && $round == 1
         correct = false
         puts "Last round!"
         puts ""
@@ -37,16 +38,16 @@ class Riddles
       end
 
       if $round == 2 && correct
-        tries = -1
+        return 'hangman'
       end
     end
-    return 'hangman'
+    puts "You lost! The man was hung by the town. You will be next..."
+    return 'death'
   end
 
   def check_answer(answer)
     if $round == 1
       if $answers[$choose_set*2] == answer
-
         return true
       end
     else
